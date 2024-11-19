2b890c617e76ca735cde279b93a4275a42d85cfc
Zephyr Moore <ZephyrAMoore@gmail.com>
2024-11-19 10:27:38 -0500

    Updated element_wheel
    Fixed element_wheel function to work for first and last elements as well.

--- battle-manager/formulas.gd
+++ battle-manager/formulas.gd
@@ -1,17 +1,25 @@
 class_name Formulas
 
 static func physical_damage(attacker, target, damage) -> int:
-    var offense = attacker.PlayerData.attack + damage
-    var defense = target.PlayerData.defense
-    var total_damage = min(0, defense - offense)
-    return total_damage * element_wheel(attacker.PlayerData.element, target.PlayerData.element)
+	var offense = attacker.PlayerData.attack + damage
+	var defense = target.PlayerData.defense
+	var total_damage = min(0, defense - offense)
+	return total_damage * element_wheel(attacker.PlayerData.element, target.PlayerData.element, attacker.PlayerData.Elements)
 
-static func element_wheel(attack_element, defend_element) -> float:
-    var multiplier = 1
-
-    if attack_element == defend_element + 1:
-        multiplier = 0.5
-    else: if attack_element == defend_element - 1:
-        multiplier = 2
-        
-    return multiplier
+static func element_wheel(attack_element, defend_element, element_list) -> float:
+	var multiplier = 1
+	
+	var next_element = attack_element + 1
+	var prev_element = attack_element - 1
+	
+	if next_element == element_list.ENDLIST:
+		next_element = 1
+	if prev_element == 0:
+		prev_element = element_list.ENDLIST - 1
+	
+	if prev_element == defend_element:
+		multiplier = 0.5
+	else: if next_element == defend_element:
+		multiplier = 2
+		
+	return multiplier
